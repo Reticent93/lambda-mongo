bcc9bd987f3759151d7bf90bbd3599e974e74ffa
gren8 <greg.renfro@gmail.com>
2021-06-29 12:14:17 -0700

    updated api

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const url = 'https://lambda-mongo.herokuapp.com/'
+// const url = 'https://reminisce-project.herokuapp.com/posts'
+// const url = 'http://localhost:8090'
 
 export const fetchPosts = () => axios.get(url)
 export const createPost = (newPost) => axios.post(url, newPost)
